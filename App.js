32191968e45ae9fe29eee4a95841004434ccf4e1
Muhammad Hassaan <muhammadhassaan@Muhammads-MacBook-Pro-3.local>
2023-01-05 16:15:15 +0000

    reproduce skia text issue

--- App.js
+++ App.js
@@ -1,21 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import { StyleSheet, Button, Text, SafeAreaView } from "react-native";
+import { Timer } from "react-native-element-timer";
 
-export default function App() {
+import { Text as SkiaText, useFont, Canvas } from "@shopify/react-native-skia";
+
+const fontFile = require("./assets/fonts/Poppins-Medium.ttf");
+const App = (_props) => {
+  const timerRef = useRef(null);
+  const font = useFont(fontFile, 20);
+
+  const [text, setText] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Timer:</Text>
+      <Timer
+        ref={timerRef}
+        style={styles.timer}
+        textStyle={styles.timerText}
+        onTimes={(e) => {
+          setText(e.toString());
+          console.log("font", font);
+        }}
+        onPause={(e) => {
+          console.log("font", font);
+        }}
+        onEnd={(e) => {}}
+      />
+
+      <Text style={styles.text}>SKIA TEXT:</Text>
+
+      <Canvas style={{ width: 200, height: 200 }}>
+        {font && (
+          <SkiaText x={10} y={15} color={"#000000"} font={font} text={text} />
+        )}
+      </Canvas>
+      <Button
+        style={styles.button}
+        title={"Start"}
+        onPress={() => {
+          console.log("font", font.getSize());
+          timerRef.current.start();
+        }}
+      />
+      <Button
+        style={styles.button}
+        title={"Pause"}
+        onPress={() => {
+          timerRef.current.pause();
+        }}
+      />
+      <Button
+        style={styles.button}
+        title={"Resume"}
+        onPress={() => {
+          timerRef.current.resume();
+        }}
+      />
+      <Button
+        style={styles.button}
+        title={"Stop"}
+        onPress={() => {
+          timerRef.current.stop();
+        }}
+      />
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    padding: 20,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 40,
+  },
+  timer: {
+    marginVertical: 10,
+  },
+  timerText: {
+    fontSize: 20,
+  },
+  button: {
+    marginVertical: 5,
+    backgroundColor: "white",
+    borderRadius: 24,
+    width: 100,
   },
 });
